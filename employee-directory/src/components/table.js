4ed175f50e0168825ed143d03352084ae97d86bf
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-08 02:17:22 -0400

    Update table.js

--- employee-directory/src/components/table.js
+++ employee-directory/src/components/table.js
@@ -65,7 +65,52 @@ filter = (e) => {
       })
 }
 
-
+render (){
+    return (
+        <div classname="container">
+            <Query filter={this.filter} />
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col"> First Name </th>
+                        <th scope="col" onClick={this.sort}> Last Name </th>
+                        <th scope="col"> City </th>
+                        <th scope="col"> County </th>
+                        <th scope="col"> Email </th>
+                        <th scope="col"> Phone Number </th>
+                        <th scope="col"> Cell Number </th>
+                      </tr>
+                  </thead>
+        <tbody>
+          {
+          this.state.filter.length > 0 ? this.state.filter.map((results) => (
+              <tr>
+                <td>{results.name.first}</td>
+                <td>{results.name.last}</td>
+                <td>{results.location.city}</td>
+                <td>{results.location.country}</td>
+                <td>{results.email}</td>
+                <td>{results.phone}</td>
+                <td>{results.cell}</td>
+              </tr>
+            )) :
+          this.state.results.length > 0 &&
+            this.state.results.map((results) => (
+              <tr>
+                <td>{results.name.first}</td>
+                <td>{results.name.last}</td>
+                <td>{results.location.city}</td>
+                <td>{results.location.country}</td>
+                <td>{results.email}</td>
+                <td>{results.phone}</td>
+                <td>{results.cell}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+        </div>
+    );
+}
 }
 
 export default companyemployeeTable
