7e8a7993a9f760b7d0bed75e63591993fe346367
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-08 02:19:04 -0400

    Update title.js

---
+++ employee-directory/src/components/title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function directoryHeader (){
+return (
+<div className="container">
+      <div className="jumbotron">
+        <h1> Company Directory </h1>
+        <p>Welcome to your company employee directory</p>
+   </div>
+   </div>     
+);
+}
+export default directoryHeader;
