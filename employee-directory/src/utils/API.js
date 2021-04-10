4a5155c267260157da74a8510551f616db68941a
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-10 19:53:55 -0400

    Update API.js

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     employeeData: function () {
-    return axios.get("https://randomuser.me/api/");
+    return axios.get("https://randomuser.me/api/?results=50");
   },
 };
